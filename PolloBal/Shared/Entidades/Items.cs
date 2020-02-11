98282fa446f4f64b2735fbf38259b838773c6614
Cuauhtemoc Olivares Pizano <c_olivares_pizano@hotmail.com>
2020-02-11 00:59:50 -0600

    Agregar archivos de proyecto.

---
+++ PolloBal/Shared/Entidades/Items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolloBal.Shared.Entidades
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string DescripcionCorta { get; set; }
+    }
+}
